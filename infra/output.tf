d5e0a7f35506cd1542273d279774093e60c5c45c
leasorssini <dlegasr@gmail.com>
2025-01-19 05:24:43 -0300

    init

--- infra/output.tf
+++ infra/output.tf
@@ -1,5 +1,17 @@
-# Output da URL do API Gateway
-output "api_url" {
-  description = "URL do API Gateway para consumo das APIs"
-  value       = "${aws_api_gateway_deployment.video_frame_pro_api_deployment.invoke_url}/prod"
+output "api_gateway_id" {
+  description = "ID do API Gateway"
+  value       = aws_api_gateway_rest_api.video_frame_pro_api.id
+}
+
+output "api_gateway_url" {
+  description = "URL do API Gateway"
+  value       = "https://${aws_api_gateway_rest_api.video_frame_pro_api.id}.execute-api.${var.aws_region}.amazonaws.com/${var.stage_name}"
+}
+
+output "resource_ids" {
+  description = "IDs dos recursos criados no API Gateway"
+  value = {
+    for k, v in aws_api_gateway_resource.resources :
+    k => v.id
+  }
 }
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,12 +1,3 @@
-# terraform.tfvars
-aws_region = "us-east-1"
-
-cognito_user_pool_id = "us-east-1_ZL87UW5Jl"
-cognito_user_pool_arn = "arn:aws:cognito-idp:us-east-1:522814708374:userpool/us-east-1_ZL87UW5Jl"
-
-auth_register_lambda_name = "user_register_function"
-auth_login_lambda_name = "user_login_function"
-
-auth_register_lambda_arn = "arn:aws:lambda:us-east-1:522814708374:function:user_register_function"
-auth_login_lambda_arn = "arn:aws:lambda:us-east-1:522814708374:function:user_login_function"
-
+aws_region  = "us-east-1"
+environment = "prod"
+stage_name  = "prod"
